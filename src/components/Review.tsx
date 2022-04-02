a8e115a96404502fb6bcf40184a399d4049f5a43
vietkhiem <khiemnvph10889@fpt.edu.vn>
2022-04-02 13:08:14 +0700

    update review

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -82,39 +82,6 @@ function Review() {
                   </div>
                 </div>
               </div>
-              <div className="carousel-item">
-                <div className="container">
-                  <div className="box">
-                    <div className="detail-box">
-                      <p>
-                        <i className="fa fa-quote-left" aria-hidden="true" />
-                      </p>
-                      <p>
-                        It is a long established fact that a reader will be
-                        distracted by the readable content of a page when
-                        looking at its layout. The point of using Lorem Ipsum is
-                        that it has a more-or-less normal distribution of
-                        letters, as opposed to using 'Content here, content
-                        here', making it lookIt is a long established fact that
-                        a reader will be distracted by the readable content of a
-                        page when looking at its layout. The point of using
-                        Lorem Ipsum is that it has a more-or-less normal
-                        distribution of letters, as opposed to using 'Content
-                        here, content here', making it look
-                      </p>
-                    </div>
-                    <div className="client-id">
-                      <div className="img-box">
-                        <img src="images/client.jpg" alt="" />
-                      </div>
-                      <div className="name">
-                        <h5>James Dew</h5>
-                        <h6>Photographer</h6>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
             {/* <div className="carousel_btn-box">
               <a

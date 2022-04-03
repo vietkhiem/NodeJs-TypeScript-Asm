eb22e2123f55c291d3a19a1850501aa2e02a53ee
vietkhiem <khiemnvph10889@fpt.edu.vn>
2022-04-03 23:42:31 +0700

    update api db

--- src/api/products.tsx
+++ src/api/products.tsx
@@ -1,45 +1,43 @@
-
-
-import { ProductType } from '../types/Product';
-import { isAuthenticate } from '../utils/localStorage';
-import instance from './instance';
+import { ProductType } from "../types/Product";
+import { isAuthenticate } from "../utils/localStorage";
+import instance from "./instance";
 
 const user = isAuthenticate();
 console.log(user);
 
 export const list = () => {
-    const url = `/products`;
-    return instance.get(url);
+  const url = `/products`;
+  return instance.get(url);
 };
 export const read = (_id: number | string) => {
-    const url = `/products/${_id}`;
-    return instance.get(url);
+  const url = `/products/${_id}`;
+  return instance.get(url);
 };
 export const add = (product: ProductType) => {
-    const url = `/products/${user?.user._id}`;
-    return instance.post(url, product, {
-        headers: {
-            "Authorization": `Bearer ${user?.token}`
-        }
-    });
+  const url = `/products/${user?.user._id}`;
+  return instance.post(url, product, {
+    headers: {
+      Authorization: `Bearer ${user?.token}`,
+    },
+  });
 };
 export const remove = (id: number | string) => {
-    const url = `/products/${user?.user._id}/${id}`;
-    return instance.delete(url, {
-        headers: {
-            "Authorization": `Bearer ${user?.token}`
-        }
-    });
+  const url = `/products/${user?.user._id}/${id}`;
+  return instance.delete(url, {
+    headers: {
+      Authorization: `Bearer ${user?.token}`,
+    },
+  });
 };
 export const update = (product: ProductType) => {
-    const url = `/products/${user?.user._id}/${product._id}`;
-    return instance.put(url, product, {
-        headers: {
-            "Authorization": `Bearer ${user?.token}`
-        }
-    });
+  const url = `/products/${user?.user._id}/${product._id}`;
+  return instance.put(url, product, {
+    headers: {
+      Authorization: `Bearer ${user?.token}`,
+    },
+  });
 };
 export const SearchProductByName = (keyword: string) => {
-    const url = `/products?name_like=${keyword}`;
-    return instance.get(url);
+  const url = `/products?name_like=${keyword}`;
+  return instance.get(url);
 };
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -14,31 +14,39 @@ const ProductList = ({ products }: ProductListProps) => {
             <h2>Our Products</h2>
           </div>
           <div className="row">
-            <div className="col-sm-6 col-lg-4">
-              <div className="box">
-                <div className="img-box">
-                  <img src="images/p7.png" alt="" />
-                  <a href="" className="add_cart_btn">
-                    <span> Add To Cart </span>
-                  </a>
-                </div>
-                <div className="detail-box">
-                  <h5>Product Name</h5>
-                  <div className="product_info">
-                    <h5>
-                      <span>$</span> 300
-                    </h5>
-                    <div className="star_container">
-                      <i className="fa fa-star" aria-hidden="true" />
-                      <i className="fa fa-star" aria-hidden="true" />
-                      <i className="fa fa-star" aria-hidden="true" />
-                      <i className="fa fa-star" aria-hidden="true" />
-                      <i className="fa fa-star" aria-hidden="true" />
+            {products?.map((product, index) => {
+              console.log(product);
+              return (
+                <div className="col-sm-6 col-lg-4">
+                  <div className="box">
+                    <div className="img-box">
+                      <NavLink to={`/products/${product._id}`}>
+                        {" "}
+                        <img src={product.img} />
+                      </NavLink>
+                      <a href="" className="add_cart_btn">
+                        <span> Add To Cart </span>
+                      </a>
+                    </div>
+                    <div className="detail-box">
+                      <h5>{product.name}</h5>
+                      <div className="product_info">
+                        <h5>
+                          <span>$</span> {product.price}
+                        </h5>
+                        <div className="star_container">
+                          <i className="fa fa-star" aria-hidden="true" />
+                          <i className="fa fa-star" aria-hidden="true" />
+                          <i className="fa fa-star" aria-hidden="true" />
+                          <i className="fa fa-star" aria-hidden="true" />
+                          <i className="fa fa-star" aria-hidden="true" />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
           <div className="btn_box">
             <a href="" className="view_more-link">

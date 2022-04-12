0029dd11479cc13fdb7efb5af68301c1c219de99
vietkhiem <khiemnvph10889@fpt.edu.vn>
2022-04-12 10:06:24 +0700

    delete banner

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,6 @@ import morgan from 'morgan';
 import productRoute from '../routes/product';
 import postRoute from '../routes/post';
 import categoryRoute from '../routes/category'
-import bannerRoute from '../routes/banner'
 import userRoute from '../routes/auth'
 import UsersRoute from '../routes/user'
 const app = express();
@@ -21,21 +20,18 @@ app.use("/api", productRoute);
 app.use("/api", postRoute);
 //route category
 app.use('/api', categoryRoute)
-    //user
+//user
 app.use('/api', userRoute)
-    // connect db
-app.use('/api', bannerRoute)
-    // route
-
+// route
 app.use('/api', UsersRoute)
+// connect db
 
-mongoose.connect('mongodb://localhost:27017/we16309')
-    // mongoose.connect('mongodb://127.0.0.1:27017/web1639')
+mongoose.connect('mongodb://localhost:27017/we16310')
 
 .then(() => console.log("Kết nối DB thành công"))
     .catch((error) => console.log(error))
     //connect
-const PORT = 3001;
+const PORT = 8000;
 app.listen(PORT, () => {
     console.log("Server is running port", PORT);
 });
